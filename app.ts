1967e407bea2041827827a2bdb9d1ad4fa6027c5
Nazianaseer77 <160523698+Nazianaseer77@users.noreply.github.com>
2024-06-25 20:42:26 +0500

    Add files via upload
    number guessing game project completed

---
+++ app.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+
+
+import inquirer from "inquirer";
+
+const randomNumber = 13;
+const answer = await inquirer.prompt([
+    {
+        name : "user Guess number",
+        type: "number",
+        message: "please guess a number",
+
+    }
+]);
+console.log
+if(answer.userGuessNumber === randomNumber){
+    console.log("congratulation! you guessed right number")
+}else{
+    console.log("you gussed a wrong number");
+}
